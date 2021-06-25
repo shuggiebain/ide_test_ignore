2b0f69332821c63aec38f32a0ea342874937feb2
Siddharth Nahar <siddharth.nahar.7@gmail.com>
2021-06-25 20:06:41 +0100

    channels - second commit

---
+++ channels/directional-channels/2/main.go
@@ -0,0 +1,16 @@
+package main //does not work
+
+import "fmt"
+
+func main() {
+	c := make(chan<- int, 2) //buffer channel allows one value to sit in there
+
+	c <- 42
+	c <- 43
+
+	//fmt.Println(<-c)
+	//fmt.Println(<-c)
+	fmt.Println("-----")
+	fmt.Printf("%T\n", c)
+
+}
